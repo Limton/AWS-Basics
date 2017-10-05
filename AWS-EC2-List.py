13f128b2285fe96d61e55441bdb2ba2eeb3c250d
Peter Xavier <limton.thi.peter-xavier@hpe.com>
2017-10-05 15:12:11 +0530

    version 1.0

---
+++ AWS-EC2-List.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.client import Config
+
+## Defining AWS Key  , when accessing from remote server.
+
+ACCESS_KEY_ID = 'AKIAJMXGZPEVMPJOG25A'
+ACCESS_SECRET_KEY = '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'
+BUCKET_NAME = 'limton'
+Region ='us-west-2'
+
+ec2 = boto3.client('ec2','us-west-2',aws_access_key_id=ACCESS_KEY_ID,
+      aws_secret_access_key=ACCESS_SECRET_KEY)
+
+#Listing all the instance running in AWS
+response = ec2.describe_instances()
+
+#Fliter all the InstanceID from AWS
+
+for Reservation in response['Reservations']:
+    for Instance in Reservation['Instances']:
+        print(Instance.get('InstanceId'))
